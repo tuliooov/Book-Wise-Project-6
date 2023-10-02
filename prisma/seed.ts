4fde38889ade5f62c88751821eb53b4c334b14ad
Marco T O Valeriano <marco.valeriano@bancointer.com.br>
2023-10-02 19:28:27 -0300

    fix seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,57 +6,57 @@ import { users } from './constants/users'
 const prisma = new PrismaClient()
 
 async function main() {
-  // await prisma.rating.deleteMany()
-  // await prisma.user.deleteMany()
-  // await prisma.categoriesOnBooks.deleteMany()
-  // await prisma.category.deleteMany()
-  // await prisma.book.deleteMany()
+  await prisma.rating.deleteMany()
+  await prisma.user.deleteMany()
+  await prisma.categoriesOnBooks.deleteMany()
+  await prisma.category.deleteMany()
+  await prisma.book.deleteMany()
 
-  // const usersSeed = users.map((user) => {
-  //   return prisma.user.create({
-  //     data: {
-  //       id: user.id,
-  //       name: user.name,
-  //       email: user.email,
-  //       avatar_url: user.avatar_url,
-  //     },
-  //   })
-  // })
+  const usersSeed = users.map((user) => {
+    return prisma.user.create({
+      data: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        avatar_url: user.avatar_url,
+      },
+    })
+  })
 
-  // const categoriesSeed = categories.map((category) => {
-  //   return prisma.category.create({
-  //     data: {
-  //       name: category.name,
-  //       id: category.id,
-  //     },
-  //   })
-  // })
+  const categoriesSeed = categories.map((category) => {
+    return prisma.category.create({
+      data: {
+        name: category.name,
+        id: category.id,
+      },
+    })
+  })
 
-  // const booksSeed = books.map((book) => {
-  //   return prisma.book.create({
-  //     data: {
-  //       id: book.id,
-  //       name: book.name,
-  //       author: book.author,
-  //       summary: book.summary,
-  //       cover_url: book.cover_url,
-  //       total_pages: book.total_pages,
-  //       categories: {
-  //         create: [
-  //           ...book.categories.map((category) => {
-  //             return {
-  //               category: {
-  //                 connect: {
-  //                   id: category.id,
-  //                 },
-  //               },
-  //             }
-  //           }),
-  //         ],
-  //       },
-  //     },
-  //   })
-  // })
+  const booksSeed = books.map((book) => {
+    return prisma.book.create({
+      data: {
+        id: book.id,
+        name: book.name,
+        author: book.author,
+        summary: book.summary,
+        cover_url: book.cover_url,
+        total_pages: book.total_pages,
+        categories: {
+          create: [
+            ...book.categories.map((category) => {
+              return {
+                category: {
+                  connect: {
+                    id: category.id,
+                  },
+                },
+              }
+            }),
+          ],
+        },
+      },
+    })
+  })
 
   const ratingsSeed = ratings.map((rating) => {
     return prisma.rating.create({
@@ -75,9 +75,9 @@ async function main() {
   })
 
   await prisma.$transaction([
-    // ...categoriesSeed,
-    // ...booksSeed,
-    // ...usersSeed,
+    ...categoriesSeed,
+    ...booksSeed,
+    ...usersSeed,
     ...ratingsSeed,
   ])
 }
